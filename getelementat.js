cf5fde5aa741c9a43bf51d6007c592e7d0c01146
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-04-26 21:04:57 +0000

    get element at index

---
+++ getelementat.js
@@ -0,0 +1,9 @@
+function getElementAt(arr, index) {
+      return arr.at(index);
+      }
+
+      // Example:
+      const myArray = [10, 20, 30, 40];
+
+      console.log(getElementAt(myArray, 0));  // 10
+      console.log(getElementAt(myArray, -1)); // 40
