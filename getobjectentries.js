4aad6b4f765ab0946149954e1afdb2d9b1b920b5
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-04-30 14:00:02 +0000

    get object entries

---
+++ getobjectentries.js
@@ -0,0 +1,7 @@
+function getObjectEntries(obj) {
+      return Object.entries(obj);
+      }
+
+      // Example:
+      console.log(getObjectEntries({ a: 1, b: 2 }));
+      // [['a', 1], ['b', 2]]
