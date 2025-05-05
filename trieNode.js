b02f1cccd94e352e7fe4c8663df299d0571bc279
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-05 19:52:29 +0000

    trie node

---
+++ trieNode.js
@@ -0,0 +1,30 @@
+class TrieNode {
+      constructor() {
+          this.children = {};
+              this.endOfWord = false;
+                }
+                }
+
+                class Trie {
+                  constructor() {
+                      this.root = new TrieNode();
+                        }
+
+                          insert(word) {
+                              let node = this.root;
+                                  for (let char of word) {
+                                        if (!node.children[char]) node.children[char] = new TrieNode();
+                                              node = node.children[char];
+                                                  }
+                                                      node.endOfWord = true;
+                                                        }
+
+                                                          search(word) {
+                                                              let node = this.root;
+                                                                  for (let char of word) {
+                                                                        if (!node.children[char]) return false;
+                                                                              node = node.children[char];
+                                                                                  }
+                                                                                      return node.endOfWord;
+                                                                                        }
+                                                                                        }
