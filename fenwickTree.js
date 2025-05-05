298337571e2ebd4dd0e064bfe5f080e8926feac0
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-05 20:03:33 +0000

    fenwick corrections

--- fenwickTree.js
+++ fenwickTree.js
@@ -3,17 +3,17 @@ class FenwickTree {
           this.tree = new Array(size + 1).fill(0);
             }
 
-              update(index, val) {
-                  while (index < this.tree.length) {
-                        this.tree[index] += val;
-                              index += index & -index;
-                                  }
-                                    }
+      update(index, val) {
+          while (index < this.tree.length) {
+          this.tree[index] += val;
+          index += index & -index;
+                         }
+                         }
 
-                                      query(index) {
-                                          let sum = 0;
-                                              while (index > 0) {
-                                                    sum += this.tree[index];
+       query(index) {
+          let sum = 0;
+          while (index > 0) {
+          sum += this.tree[index];
                                                           index -= index & -index;
                                                               }
                                                                   return sum;
