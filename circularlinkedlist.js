871c30d7168daf526453dd3f63e4c936b2c7ce1d
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-09 17:23:39 +0000

    mod circular

--- circularlinkedlist.js
+++ circularlinkedlist.js
@@ -10,21 +10,17 @@ class  CircularLinkedList {
        }
 
 append(data) {
-  const newNode = new CircularNode(data);
-  
-    
-                                                                          
-                            current.next = newNode;
-                                                                                  newNode.next = this.head;
-                                                                                    }
+  const newNode = new CircularNode(data);                                                                      
+  current.next = newNode;
+  newNode.next = this.head;
+                }
 
-                                                      printList() {
-                                                                                          if (!this.head) return;
-
-                                                                                              let current = this.head;
-                                                                                                  do {
-                                                                                                        console.log(current.data);
-                                                                                                              current = current.next;
-                                                                                                                  } while (current !== this.head);
-                                                                                                                    }
-                                                                                                                    }
+printList() {
+   if (!this.head) return;
+   let current = this.head;
+   do {
+     console.log(current.data);
+     current = current.next;
+        } while (current !== this.head);
+                          }
+                      }
