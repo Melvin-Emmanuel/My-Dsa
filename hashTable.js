ee2f5239261df6bcb68fa85883ddac846dad078f
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-18 21:43:25 +0000

    final hash mod

--- hashTable.js
+++ hashTable.js
@@ -20,9 +20,9 @@ class HashTable {
              const bucket = this.buckets[index];
              if (!bucket) return undefined;
 
-                                                                for (let [k, v] of bucket) {
-                                                                      if (k === key) return v;
-                                                                          }
-                                                                            }
-                                                                            }
+            for (let [k, v] of bucket) {
+                if (k === key) return v;
+                              }
+                     }
+            }
 }
