2cb94826a3f263ef8637e43aac62723ba850f6c8
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-29 19:45:50 +0000

    modnew rotatematr

--- rotatematrix.js
+++ rotatematrix.js
@@ -1,14 +1,14 @@
 function rotate(matrix) {
-      const n = matrix.length;
+   const n = matrix.length;
 
         // Transpose
-          for (let i = 0; i < n; i++) {
-              for (let j = i; j < n; j++) {
-                    [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
-                        }
-                          }
+   for (let i = 0; i < n; i++) {
+      for (let j = i; j < n; j++) {
+          [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+         }
+    }
 
-                            // Reverse each row
+                      
                               for (let row of matrix) {
                                   row.reverse();
                                     }
