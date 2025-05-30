567af4dcaabf881f381c5f522a12767fc0c51c1d
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-05-30 14:29:19 +0000

    correctfifo

--- fifoqueue.js
+++ fifoqueue.js
@@ -1,20 +1,3 @@
-class Queue {
-      constructor() {
-          this.items = [];
-            }
-
-              enqueue(item) {
-                  this.items.push(item);
-                    }
-
-                      dequeue() {
-                          return this.items.shift();
-                            }
-
-                              peek() {
-                                  return this.items[0];
-                                    }
-                                    }
 class  Queue {
   constructor() {
     this.items = [];
